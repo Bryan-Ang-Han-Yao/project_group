d85a6b4e393034f6d12716f1a6a04b8b44a043dc
nataleelow <S10258447@connect.np.edu.sg>
2023-07-29 14:43:17 +0800

    overheads indentifier update

--- overheads.py
+++ overheads.py
@@ -1,1 +1,14 @@
 # Assigned to Natalee
+
+from pathlib import Path
+import csv
+
+file_path = Path.cwd()/"csv_reports"/"Overheads.csv"
+
+def overheads_identifier():
+    with file_path.open(mode="r", encoding="UTF-8", newline="") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        overheadsRecords = [] 
+        for row in csv_reader:
+            overhead_category = row[0].strip()
