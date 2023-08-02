23f1633cec545905b94f177de04b9708e6feb89b
Chia-Jun-Xiang <S10262064@connect.np.edu.sg>
2023-08-02 16:46:42 +0800

    Comments for Retrieving Highest Overhead Value

--- overheads.py
+++ overheads.py
@@ -22,13 +22,33 @@ def overheads_identifier():
 
         # Iterates through the rows in the "Overheads.csv" file
         for row in csv_reader:
+
+            # Retrieve the overhead category for each record
             overhead_category = row[0].strip()
+
+            # Retrieve the overheard percentage for each record and converting it to a float
             overhead_percentage = float(row[1])
+
+            # Appends the overhead category and overheard percentage to the "overheadsRecords" list
             overheadsRecords.append([overhead_category, overhead_percentage])
+
+        # Finds the row with the highest value in the overheads column
         highest_overhead = max(overheadsRecords, key=overheadCol)
+
+        # Retrieves the highest overhead category name
         highest_category = highest_overhead[0]   
+
+        # Retrieves the highest overhead value
         highest_percentage = highest_overhead[1]
+
+        # Returns the formatted output of the highest overhead using the 'f' string
         return f"[HIGHEST OVERHEAD] {highest_category}: {highest_percentage}%\n"
     
 def overheadCol(overheadRow):
+    """
+    - Function to obtain the second item in each row of the CSV file
+    - "overheadRow" is a parameter that takes in each row in the "Overheads.csv" file
+    """
+
+    # Retrieves the second item of each row in the "Overheads.csv" file (Overhead value)
     return overheadRow[1]
