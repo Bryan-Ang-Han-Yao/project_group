13579d4a332005e87a3b118b47045da3abce2d90
nuh669 <S10259340@connect.np.edu.sg>
2023-08-03 22:40:07 +0800

    Finds the days with a net profit deficit/surplus

--- profit_loss.py
+++ profit_loss.py
@@ -3,3 +3,11 @@ import csv
 
 file_path = Path.cwd()/"csv_reports"/"Profits_and_Loss.csv"
 
+def Profit_and_Loss_identifier():
+    with file_path.open(mode = 'r', encoding="UTF-8", newline="") as csv_file:
+
+        csv_reader = csv.reader(csv_file)
+
+        net_profit_deficit_days = []
+
+        prev_day_net_profit = 0
