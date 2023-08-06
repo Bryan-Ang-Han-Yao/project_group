e1fb44c934f99b1c7e9529976352271aaca371d9
nuh669 <S10259340@connect.np.edu.sg>
2023-08-06 22:27:52 +0800

    converting day number from CSV into an integer

--- profit_loss.py
+++ profit_loss.py
@@ -11,3 +11,15 @@ def Profit_and_Loss_identifier():
         net_profit_deficit_days = []
 
         prev_day_net_profit = 0
+
+        net_profit_highest_surplus = 0
+
+        for row in csv_reader:
+            
+            day = int(row[0])
+
+            net_profit = int(row[4])
+
+            if net_profit < prev_day_net_profit and prev_day_net_profit != 0:
+
+                net_profit_deficit = prev_day_net_profit - net_profit
