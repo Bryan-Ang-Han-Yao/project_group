66b0d21b60a166c0672b4dfd8c2822d610b24d86
Chia-Jun-Xiang <S10262064@connect.np.edu.sg>
2023-08-08 13:12:51 +0800

    Creating List and Variable to Store Data

--- profit_loss.py
+++ profit_loss.py
@@ -1,17 +1,29 @@
 from pathlib import Path
 import csv
 
+# Creates a path to csv file
 file_path = Path.cwd()/"csv_reports"/"Profits_and_Loss.csv"
 
 def Profit_and_Loss_identifier():
-    with file_path.open(mode = 'r', encoding="UTF-8", newline="") as csv_file:
+    """
+    - Identifies the day(s) with Net Profit deficit or day with highest Net Profit surplus
+    - No parameter is required
+    """
 
+    # Opens and reads the csv file
+    with file_path.open(mode = 'r', encoding="UTF-8", newline="") as csv_file:
         csv_reader = csv.reader(csv_file)
 
+        # Skips the header
+        next(csv_reader)
+
+        # Create an empty lists to store days with Net Profit deficit
         net_profit_deficit_days = []
 
+        # Create a variable to store the previous day Net Profit
         prev_day_net_profit = 0
 
+        # Create a variable to store the highest surplus in Net Profit
         net_profit_highest_surplus = 0
 
         for row in csv_reader:
