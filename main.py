793aadba62406754437cea263239d676f43b6bfd
Bryan-Ang-Han-Yao <S10262640@connect.np.edu.sg>
2023-08-09 17:41:47 +0800

    Opening file in write mode alongside encoding

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ net_profit_value = profit_loss.Profit_and_Loss_identifier()
 
 # Declaring a variable to represent the full path to the "summary_report.txt" file in the current working directory
 wp = Path.cwd()/"summary_report.txt"
+
+# Opening the file in write mode ("w") and specifying "UTF-8" as the character encoding for the file
+with wp.open(mode = "w", encoding = "UTF-8") as file:
+    
