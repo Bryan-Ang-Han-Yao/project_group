3e2a66f8df34f2b76eb54ffe909c6c89850405ee
Bryan-Ang-Han-Yao <S10262640@connect.np.edu.sg>
2023-08-09 15:50:29 +0800

    Declaring variables to store modularized functions

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import overheads, cash_on_hand, profit_loss
 from pathlib import Path
+
+# Declaring variables to store the modularized functions
+overheads_value = overheads.overheads_identifier()
+cash_on_hand_value = cash_on_hand.COH_identifier()
+net_profit_value = profit_loss.Profit_and_Loss_identifier()
